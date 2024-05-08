99c9945c9be9f815defe57d793f893fc4a2b69ea
Srikant Kumar Gouda <144648412+SrikantKumarGouda@users.noreply.github.com>
2024-05-08 14:44:28 +0530

    Add files via upload

---
+++ AudioAura/src/main/java/com/kodnest/tunehub/AudioAuraApplication.java
@@ -0,0 +1,13 @@
+package com.kodnest.tunehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioAuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioAuraApplication.class, args);
+	}
+
+}
